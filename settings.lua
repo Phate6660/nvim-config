84437a6cc47556f3e362caf19d2830f5e2c48569
ValleyKnight <ValleyKnight@protonmail.com>
2021-06-25 01:37:05 -0400

    update settings.lua to use vim.opt_local and vim.opt_global

--- settings.lua
+++ settings.lua
@@ -1,47 +1,46 @@
 --- Helpers
-local b = vim.bo     -- Access to buffer-local options
-local cmd = vim.cmd  -- To execute Vim commands e.g. cmd('pwd')
-local g = vim.g      -- Access to global options (can be non-existant, e.g. a plugin's settings)
-local o = vim.o      -- Access to global options (has to exist in base nvim)
-local w = vim.wo     -- Access to window-local options
+local cmd = vim.cmd      -- To execute Vim commands e.g. cmd('pwd')
+local g = vim.opt_global -- Acces to global options (must exist)
+local G = vim.g          -- Access to global options (can be non-existant, e.g. a plugin's settings)
+local o = vim.opt_local  -- Access to local options (must exist)
 
 --- Settings
 -- Expand tabs into spaces
-cmd('set expandtab')
+o.expandtab = true
 local indent, width = 4, 100
 -- Set size of indent to 4
-b.shiftwidth = indent
+o.shiftwidth = indent
 -- Insert indents automatically
-b.smartindent = true
+o.smartindent = true
 -- Tabs = 4 spaces
-b.tabstop = indent
--- Use the dark variant of the neon colorscheme
-g.neon_style = "doom" -- Supported options: 'normal', 'doom', 'dark', 'light'
--- Syntastic options
-g.syntastic_always_populate_loc_list = 1
-g.syntastic_auto_loc_list = 1
-g.syntastic_check_on_open = 1
-g.syntastic_check_on_wq = 0
+o.tabstop = indent
 -- Completion options required for compe
-o.completeopt = 'menuone,noselect'
+g.completeopt = 'menuone,noselect'
 -- Enable mouse support
-o.mouse = 'a'
+g.mouse = 'a'
 -- Put new windows below current
-o.splitbelow = true
+g.splitbelow = true
 -- Put new windows to the right of the current
-o.splitright = true
+g.splitright = true
 -- Enable truecolor suppor
-o.termguicolors = true
+g.termguicolors = true
+-- Use the dark variant of the neon colorscheme
+G.neon_style = "doom" -- Supported options: 'normal', 'doom', 'dark', 'light'
+-- Syntastic options
+G.syntastic_always_populate_loc_list = 1
+G.syntastic_auto_loc_list = 1
+G.syntastic_check_on_open = 1
+G.syntastic_check_on_wq = 0
 -- Color the 100th column of text
-w.colorcolumn = tostring(width)
+o.colorcolumn = tostring(width)
 -- Highlight the current line the cursor is on
-w.cursorline = true
+o.cursorline = true
 -- Show line numbers
-w.number = true
+o.number = true
 -- Show relative line numbers
-w.relativenumber = true
+o.relativenumber = true
 -- DIsable line wrapping
-w.wrap = false
+o.wrap = false
 
 -- Set the colorscheme to neon (installed through paq)
 cmd('colorscheme neon')
